a865da09c7abdfd57fda49c84ba76fd9f287d8d7
Thorpee545 <136597631+Thorpee545@users.noreply.github.com>
2025-12-24 07:28:15 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://customswebailtelstr-zsbcn.framer.website"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://xn--customwebailtelstr-zsb.framer.website"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
